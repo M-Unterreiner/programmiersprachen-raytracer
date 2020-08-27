d3ff3e6d63454c7ee5a693d86ed3354e94c1110c
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-08-27 17:26:36 +0200

    Added shape test

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,19 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+#include "../framework/color.hpp"
+#include "../framework/shape.hpp"
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
+}
+
+
+TEST_CASE ("Shape")
+{
+  SECTION("Default constructor")
+  {
+    Shape s1{};
+  }
 }
