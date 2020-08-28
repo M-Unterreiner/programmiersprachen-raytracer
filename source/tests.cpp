ddc56d54c1f2ea3fd8aeb6e04c2aa9d8a95c222f
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-08-28 16:45:19 +0200

    Added tests for Box

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,8 +1,10 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+#include "../framework/box.hpp"
 #include "../framework/color.hpp"
 #include "../framework/shape.hpp"
+#include "../framework/sphere.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -15,5 +17,22 @@ TEST_CASE ("Shape")
   SECTION("Default constructor")
   {
     Shape s1{};
+  }
+}
+
+TEST_CASE ("Sphere")
+{
+  SECTION("Default constructor")
+  {
+    Sphere s1{};
+  }
+
+}
+
+TEST_CASE ("Box")
+{
+  SECTION("Default constructor")
+  {
+    Box b1{};
   }
 }
