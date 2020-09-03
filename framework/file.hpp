bd6a96d479e7cdbf2823210abf9901a20080ed06
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-09-03 17:59:27 +0200

    Concenated file_ from filename with filedirectory

--- framework/file.hpp
+++ framework/file.hpp
@@ -10,24 +10,31 @@ struct File
 {
   std::string filename_;
   std::string filedirectory_;
+  std::string file_;
 
 File():
 filename_{"example.sdf"},
-filedirectory_{"../../ressources/"}
+filedirectory_{"../../ressources/"},
+file_{filedirectory_ + filename_}
 {
+  std::cout << "filename and directory wasn't set. " << file_ << " was used" << std::endl;
 }
 
+// TODO: If file doesn't exist it should use a fallback solution
 File(std::string filename):
 filename_(filename),
 filedirectory_{"../../ressources/"}
 {
-  std::cout << "file directory wasn't set. " << filedirectory_ << " was used" << std::endl;
+  file_ = filedirectory_ + filename_;
+  std::cout << "file directory wasn't set. " << file_ << " was used" << std::endl;
 }
 
 File(std::string filename, std::string filedirectory):
 filename_(filename),
 filedirectory_(filedirectory)
 {
+  file_ = filedirectory_ + filename_;
+  std::cout << "file was set to " << file_ << std::endl;
 }
 
 void print_file()
