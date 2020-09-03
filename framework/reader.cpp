bafba0cc059560e34cf9176ea8baec663b7efa82
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-09-03 18:00:02 +0200

    Added reader with tests

--- framework/reader.cpp
+++ framework/reader.cpp
@@ -1,5 +1,9 @@
 #include <memory>
 
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
 #include "reader.hpp"
 #include "file.hpp"
 
@@ -18,7 +22,7 @@ Reader::~Reader()
 }
 
 
-void Reader::set_File(std::shared_ptr<File> file)
+void Reader::set_file(std::shared_ptr<File> file)
 {
   file_ = std::move(file);
 }
@@ -26,4 +30,62 @@ void Reader::set_File(std::shared_ptr<File> file)
 std::string Reader::get_filename()
 {
   return file_->filename_;
+}
+
+/*
+ read_file reads the file
+*/
+void Reader::read_file()
+{
+  std::string example = file_->file_;
+
+  std::ifstream filestream;
+  filestream.open(file_->file_);
+  std::string buffer;
+
+  while(std::getline(filestream, buffer))
+  {
+    std::cout << "While" << std::endl;
+
+    std::stringstream stream (buffer);
+    std::string keyword;
+
+    stream >> keyword;
+
+    if ("define" == keyword)
+    {
+      std::cout << "define" << std::endl;
+
+      if ("material" == keyword)
+      {
+        std::cout << "material" << std::endl;
+      }
+
+      if ("shape" == keyword)
+      {
+        std::cout << "shape" << std::endl;
+
+        if ("box" == keyword)
+        {
+          std::cout << "box" << std::endl;
+        }
+
+        if ("sphere" == keyword)
+        {
+          std::cout << "sphere" << std::endl;
+        }
+
+        if ("composite" == keyword)
+        {
+          std::cout << "composite" << std::endl;
+        }
+      }
+
+      if ("camera" == keyword)
+      {
+        std::cout << "camera" << std::endl;
+      }
+
+    }
+  }
 }
--- framework/reader.hpp
+++ framework/reader.hpp
@@ -1,10 +1,12 @@
 #ifndef READER_HPP
 #define READER_HPP
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 #include <memory>
 #include <string>
-#include <sstream>
-#include <iostream>
 #include "file.hpp"
 
 class Reader
@@ -14,8 +16,8 @@ class Reader
   Reader();
   Reader(std::shared_ptr<File> filename);
   ~Reader();
-  void set_File(std::shared_ptr<File> filename);
-  void read_File();
+  void set_file(std::shared_ptr<File> filename);
+  void read_file();
   std::string get_filename();
 
 
