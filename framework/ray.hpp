8b3f949fdb6bd8753891624911771006da8ed825
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-09-09 21:33:17 +0200

    Added default constructor for ray

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -6,10 +6,20 @@
 
 struct Ray
 {
-  glm::vec3 origin = {0.0f, 0.0f, 0.0f};
-  glm::vec3 direction = {0.0f, 0.0f, -1.0f};
+  glm::vec3 origin_ = {0.0f, 0.0f, 0.0f};
+  glm::vec3 direction_ = {0.0f, 0.0f, -1.0f};
 
-  // Does Ray need a constructor even it is a DTO? 
+  // Does Ray need a constructor even it is a DTO?
+
+  Ray():
+    origin_{0.0f, 0.0f, 0.0f},
+    direction_{0.0f, 0.0f, -1.0f}
+  {}
+
+  Ray(glm::vec3 origin_, glm::vec3 direction): 
+  origin_{origin_},
+  direction_{glm::normalize(direction)} 
+  {}
 };
 
 #endif 
