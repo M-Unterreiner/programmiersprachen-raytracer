7ebe39af0b8bbc98e58bae2cdde8094a5dc6b08d
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-09-09 18:20:24 +0200

    Added print_light

--- framework/light.hpp
+++ framework/light.hpp
@@ -11,6 +11,13 @@ struct Light
   Color color_ {0.0f, 0.0f, 0.0f};
   glm::vec3 position_ {0,0,0};
   unsigned int brightness_ {0};
+
+  void print_light()
+  {
+    std::cout << name_ << std::endl;
+    std::cout << "r: " << color_.r << " g: "<< color_.g << " b: " << color_.b << std::endl;
+  }
+
 };
 
 
